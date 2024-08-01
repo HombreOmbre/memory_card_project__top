9255fd689060bcc80bf0659b5426f8510ecd6099
HombreOmbre <marcin.obrebowski@outlook.com>
2024-08-01 18:26:38 +0200

    Create all necessary useStates, useEffect for fetching pokemons and add HeaderComponent, FooterComponent and GameComponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
+import {useEffect, useState} from "react";
+import ApiCall from "./components/ApiCall.jsx";
+import HeaderComponent from "./components/HeaderComponent.jsx";
+import FooterComponent from "./components/FooterComponent.jsx";
+import GameComponent from "./components/GameComponent.jsx";
+import './styles/App.scss';
+
 export default function App() {
-  return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  )
+    const pokemonUrl = 'https://pokeapi.co/api/v2/pokemon';
+
+    const [pokemons, setPokemons] = useState([]);
+    const [score, setScore] = useState(0);
+    const [bestScore, setBestScore] = useState(0);
+
+    useEffect(() => {
+        const offset = Math.floor(Math.random() * 500);
+        ApiCall.getPokemonsArray(pokemonUrl, setPokemons, offset);
+    },[]);
+
+    return (
+        <div className='app-container'>
+            <HeaderComponent
+                score={score}
+                bestScore={bestScore}
+            />
+            <main className='app-container__section'>
+                <GameComponent
+                    pokemons={pokemons}
+                    score={score}
+                    setScore={setScore}
+                    bestScore={bestScore}
+                    setBestScore={setBestScore}
+                />
+            </main>
+            <FooterComponent />
+        </div>
+    );
 }
